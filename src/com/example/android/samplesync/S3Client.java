b4ec3acf29a7f98c0f50ab22ffb6d98f616f000a
snackypants <snackypants@gmail.com>
2011-02-23 15:39:31 -0800

    Javadoc. Fix compile errors. Use more accurate "path" instead of "uri". Fix interface of sign to include key (!). Added TODO: Actually do some signing.

--- src/com/example/android/samplesync/S3Client.java
+++ src/com/example/android/samplesync/S3Client.java
@@ -28,7 +28,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 
 /**
- * TODO Javadoc
+ * A very simple Amazon S3 client.
  */
 public class S3Client {
 
@@ -41,18 +41,19 @@ public class S3Client {
 
 
     /**
-     * @param uri
-     * @param date
-     * @param method
-     * @param signature
+     * @param path The path component of the URL to connect to (e.g.
+     * "/bucket/object/name").
+     * @param date A timestamp ({@see #timestamp}).
+     * @param method The HTTP method, e.g. "PUT" or "GET".
+     * @param signature The signature ({@see #sign}).
      *
      * @return A connection.
      */
-    private static HttpURLConnection connection(String uri, String date, String method,
-                           String signature)
+    private static HttpURLConnection connection(String path, String date, String method,
+                                                String signature)
         throws MalformedURLException, IOException
     {
-        URL url = new URL(PROTOCOL, HOSTNAME, PORT, uri);
+        URL url = new URL(PROTOCOL, HOSTNAME, PORT, path);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
         conn.setDoInput(true);
@@ -69,24 +70,29 @@ public class S3Client {
 
 
     /**
-     * TODO Javadoc
+     * @param key The secret S3 key.
+     * @param keyId The secret S3 key ID.
+     * @param bucketName The name of the bucket to create.
+     *
+     * @return The HTTP status code for the request.
      */
     public static int createBucket(String key, String keyId, String bucketName) {
-        String uri = "/" + bucketName;
+        String path = "/" + bucketName;
         String date = timestamp();
         String method = "PUT";
-        String signature = sign(keyId, (String []) { method, "", "", date, uri });
+        String [] toSign = { method, "", "", date, path };
+        String signature = sign(key, keyId, toSign);
 
-        int status;
+        int status = 400;
 
         try {
-            HttpURLConnection = connection(uri, date, method, signature);
+            HttpURLConnection conn = connection(path, date, method, signature);
             conn.setRequestProperty("Content-Length", "0");
             status = conn.getResponseCode();
 
             if (200 != status) {
-                Log.i(LOG_TAG, "Status is " + statusCode);
-                String resp = httpConn.getResponseMessage();
+                Log.i(LOG_TAG, "Status is " + status);
+                String resp = conn.getResponseMessage();
                 Log.i(LOG_TAG, resp);
             } else
                 Log.i(LOG_TAG, "Bucket created successfully, probably");
@@ -101,24 +107,30 @@ public class S3Client {
 
 
     /**
-     * TODO Javadoc
+     * @param key The secret S3 key.
+     * @param keyId The secret S3 key ID.
+     * @param bucketName The name of the bucket that contains the object.
+     * @param objectName The name of the object to retrieve.
+     *
+     * @return The HTTP status code for the request.
      */
     public static int getObject(String key, String keyId, String bucketName, String objectName) {
-        String uri = "/" + bucketName + "/" + objectName;
+        String path = "/" + bucketName + "/" + objectName;
         String date = timestamp();
         String method = "GET";
-        String signature = sign(keyId, (String []) { method, "", "", date, uri });
+        String [] toSign = { method, "", "", date, path };
+        String signature = sign(key, keyId, toSign);
 
-        int status;
+        int status = 400;
 
         try {
-            HttpURLConnection = connection(uri, date, method, signature);
+            HttpURLConnection conn = connection(path, date, method, signature);
             conn.setRequestProperty("Content-Length", "0");
             status = conn.getResponseCode();
 
             if (200 != status) {
-                Log.i(LOG_TAG, "Status is " + statusCode);
-                String resp = httpConn.getResponseMessage();
+                Log.i(LOG_TAG, "Status is " + status);
+                String resp = conn.getResponseMessage();
                 Log.i(LOG_TAG, resp);
             } else
                 Log.i(LOG_TAG, "Here is your object, probably");
@@ -133,29 +145,37 @@ public class S3Client {
 
 
     /**
-     * TODO Javadoc
+     * @param key The secret S3 key.
+     * @param keyId The secret S3 key ID.
+     * @param bucketName The name of the bucket in which to create the
+     * object.
+     * @param objectName The name of the object to create.
+     * @param data The data to store in the object.
+     *
+     * @return The HTTP status code for the request.
      */
     public static int createObject(String key, String keyId, String bucketName, String objectName, byte [] data) {
-        String uri = "/" + bucketName + "/" + objectName;
+        String path = "/" + bucketName + "/" + objectName;
         String date = timestamp();
         String method = "PUT";
         String contentType = "text/plain";
-        String signature = sign(keyId, (String []) { method, "", contentType, date, uri });
+        String [] toSign = { method, "", contentType, date, path };
+        String signature = sign(key, keyId, toSign);
 
-        int status;
+        int status = 400;
 
         try {
-            HttpURLConnection = connection(uri, date, method, signature);
+            HttpURLConnection conn = connection(path, date, method, signature);
 
             String body = "data=" + readFile(objectName);
 
-            conn.setRequestProperty("Content-Length", body.getBytes("UTF-8").length);
+            conn.setRequestProperty("Content-Length", "" + body.getBytes("UTF-8").length);
             conn.setRequestProperty("Content-Type", contentType);
             status = conn.getResponseCode();
 
             if (200 != status) {
-                Log.i(LOG_TAG, "Status is " + statusCode);
-                String resp = httpConn.getResponseMessage();
+                Log.i(LOG_TAG, "Status is " + status);
+                String resp = conn.getResponseMessage();
                 Log.i(LOG_TAG, resp);
             } else
                 Log.i(LOG_TAG, "Object stored, probably");
@@ -170,11 +190,15 @@ public class S3Client {
 
 
     /**
-     * @param items TODO explain
+     * @param key The secret S3 key.
+     * @param keyId The secret S3 key ID.
+     * @param items The data items that Amazon S3 requests require to be
+     * signed. Should be an array of length 5: { method, MD5, content-type,
+     * timestamp, path }.
      *
      * @return A signature in the format S3 requires.
      */
-    private static String sign(String keyId, String [] items) {
+    private static String sign(String key, String keyId, String [] items) {
         StringBuffer buf = new StringBuffer();
 
         for (int i = 0; i < items.length; i++) {
@@ -182,6 +206,8 @@ public class S3Client {
             if (i < items.length - 1)
                 buf.append("\n");
         }
+
+        // TODO: Sign here!
 
         return "AWS " + keyId + ":" + buf.toString();
     }
@@ -197,6 +223,11 @@ public class S3Client {
     }
 
 
+    /**
+     * @param pathname A relative or absolute filesystem pathname.
+     *
+     * @return The contents of the named file as a UTF-8 string.
+     */
     private static String readFile(String filename) throws Exception {
         try {
             FileInputStream fin = new FileInputStream(filename);
@@ -215,13 +246,18 @@ public class S3Client {
         }
     }
 
-    private static void setStackTrace(Throwable aThrowable) {
+
+    /**
+     * Logs a stack trace to the Android log.
+     *
+     * @param t The source of the stack trace.
+     */
+    private static void setStackTrace(Throwable t) {
         final Writer result = new StringWriter();
         final PrintWriter printWriter = new PrintWriter(result);
-        aThrowable.printStackTrace(printWriter);
-        Log.i(LOG_TAG, result.toString());
+        t.printStackTrace(printWriter);
+        Log.d(LOG_TAG, result.toString());
     }
-    
  
 }
 
