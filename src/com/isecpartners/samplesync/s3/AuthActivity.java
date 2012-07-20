7c506ca3ec96e8b485ec2ad2069452097aa7d87a
mthambireddy isec <mthambireddy.isec@gmail.com>
2012-07-20 00:33:35 -0700

    updated  code to use constants

--- src/com/isecpartners/samplesync/s3/AuthActivity.java
+++ src/com/isecpartners/samplesync/s3/AuthActivity.java
@@ -11,7 +11,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.provider.ContactsContract;
 import android.util.Log;
